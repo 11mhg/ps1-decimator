db3f51ac8ead9ffcc4079019964aa822bf17ea77
Hamada <11mhg@queensu.ca>
2025-09-06 19:58:06 -0400

    fix

--- ps1_decimator/__init__.py
+++ ps1_decimator/__init__.py
@@ -6,6 +6,7 @@ from . import operators, ui
 
 LOGGER = logging.getLogger(__name__)
 
+# Support live-reload in Blender's text editor
 if "bpy" in locals():
     importlib.reload(operators)
     importlib.reload(ui)
@@ -15,11 +16,12 @@ def register() -> None:
     """Register all add-on classes and UI."""
     operators.register()
     ui.register()
-    print("Registered")
+    print("PS1 Decimator: registered")
 
 
 def unregister() -> None:
     """Unregister all add-on classes and UI."""
     ui.unregister()
     operators.unregister()
-    print("Unregistered")
+    print("PS1 Decimator: unregistered")
+
